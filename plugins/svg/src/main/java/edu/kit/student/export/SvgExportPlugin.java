e76d37e84aa79605d1e4bf5407c0d60b0d39fc3c
Lucas Steinmann <lucas.steinmann@gmail.com>
2016-06-28 23:16:53 +0200

    Refactored svg-package to mach checkstyle.

---
+++ plugins/svg/src/main/java/edu/kit/student/export/SvgExportPlugin.java
@@ -0,0 +1,61 @@
+package edu.kit.student.export;
+
+
+import edu.kit.student.graphmodel.Edge;
+import edu.kit.student.graphmodel.Vertex;
+import edu.kit.student.plugin.EdgeFilter;
+import edu.kit.student.plugin.Exporter;
+import edu.kit.student.plugin.Importer;
+import edu.kit.student.plugin.Plugin;
+import edu.kit.student.plugin.VertexFilter;
+import edu.kit.student.plugin.WorkspaceOption;
+
+import java.util.List;
+
+/**
+ * This class provides the {@link SvgExporter} to the plugin manager.
+ */
+public class SvgExportPlugin implements Plugin {
+
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void load() {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public List<WorkspaceOption> getWorkspaceOptions() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<VertexFilter<? extends Vertex>> getVertexFilter() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<EdgeFilter<? extends Edge<? extends Vertex>, ? extends Vertex>> getEdgeFilter() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Exporter> getExporter() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public List<Importer> getImporter() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+}
--- plugins/svg/src/main/java/edu/kit/student/export/SvgExporter.java
+++ plugins/svg/src/main/java/edu/kit/student/export/SvgExporter.java
@@ -1,28 +1,29 @@
 package edu.kit.student.export;
 
-import java.io.FileOutputStream;
 
 import edu.kit.student.graphmodel.SerializedGraph;
 import edu.kit.student.plugin.Exporter;
 
+import java.io.FileOutputStream;
+
 public class SvgExporter implements Exporter {
 
-	@Override
-	public String getSupportedFileEnding() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public String getSupportedFileEnding() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public String getName() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public void exportGraph(SerializedGraph graph, FileOutputStream filestream) {
-		// TODO Auto-generated method stub
-		
-	}
+    @Override
+    public void exportGraph(SerializedGraph graph, FileOutputStream filestream) {
+        // TODO Auto-generated method stub
+        
+    }
 
 }
