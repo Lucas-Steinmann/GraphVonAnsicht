452fab678489db4d962a5efc20f0bd8ca0e90308
Lucas Steinmann <lucas.steinmann@gmail.com>
2016-06-30 22:08:40 +0200

    Renamed service declaration from SVGPlugin. Start Implementation Joana.

--- plugins/joana/src/main/java/edu/kit/student/joana/JoanaGraph.java
+++ plugins/joana/src/main/java/edu/kit/student/joana/JoanaGraph.java
@@ -1,59 +1,96 @@
 package edu.kit.student.joana;
 
-import java.util.List;
-
-import edu.kit.student.graphmodel.directed.DefaultDirectedGraph;
 import edu.kit.student.graphmodel.LayeredGraph;
+import edu.kit.student.graphmodel.directed.DefaultDirectedGraph;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Set;
+
 
 /**
  * An abstract superclass for all JOANA specific graphs.
  */
-public abstract class JoanaGraph extends DefaultDirectedGraph<JoanaVertex, JoanaEdge> implements LayeredGraph<JoanaVertex, JoanaEdge> {
+public abstract class JoanaGraph 
+    extends DefaultDirectedGraph<JoanaVertex, JoanaEdge> 
+    implements LayeredGraph<JoanaVertex, JoanaEdge> {
+    
 
     public JoanaGraph(String name, Integer id) {
+        this(name, id, null, null);
+    }
+    
+    public JoanaGraph(String name, Integer id, Set<JoanaVertex> vertices, Set<JoanaEdge> edges) {
         super(name, id);
-        // TODO Auto-generated constructor stub
     }
 
     @Override
-	public int getLayerCount() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public int getLayerCount() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public int getVertexCount(int layerNum) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int getVertexCount(int layerNum) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public int getLayer(JoanaVertex vertex) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int getLayer(JoanaVertex vertex) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public List<JoanaVertex> getLayer(int layerNum) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<JoanaVertex> getLayer(int layerNum) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public List<List<JoanaVertex>> getLayers() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public List<List<JoanaVertex>> getLayers() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public int getHeight() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int getHeight() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
 
-	@Override
-	public int getMaxWidth() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public int getMaxWidth() {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+    
+    private class Layering {
+        
+        public int getLayer(JoanaVertex vertex) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        public List<JoanaVertex> getLayer(int layerNum) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        public List<List<JoanaVertex>> getLayers() {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        public int getHeight() {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        public int getMaxWidth() {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+    }
 }
--- plugins/joana/src/main/java/edu/kit/student/joana/methodgraph/MethodGraph.java
+++ plugins/joana/src/main/java/edu/kit/student/joana/methodgraph/MethodGraph.java
@@ -1,8 +1,5 @@
 package edu.kit.student.joana.methodgraph;
 
-import java.util.List;
-import java.util.Set;
-
 import edu.kit.student.graphmodel.CompoundVertex;
 import edu.kit.student.graphmodel.LayeredGraph;
 import edu.kit.student.graphmodel.Vertex;
@@ -13,84 +10,107 @@ import edu.kit.student.joana.JoanaVertex;
 import edu.kit.student.plugin.LayoutOption;
 import edu.kit.student.plugin.LayoutRegister;
 
+import java.util.List;
+import java.util.Set;
+
 /**
- * This is a specific graph representation for a MethodGraph in JOANA 
+ * This is a specific graph representation for a MethodGraph in JOANA .
  */
 public class MethodGraph extends JoanaGraph {
 
-	public MethodGraph(String name, Integer id) {
-        super(name, id);
-        // TODO Auto-generated constructor stub
+    private static final String ENTRY_NAME = "Entry";
+    private static LayoutRegister<MethodGraphLayoutOption> register;
+    private JoanaVertex entry;
+    private List<FieldAccess> fieldAccesess;
+
+//    private MethodGraph(String name, Integer id) {
+//        super(name, id);
+//    }
+
+    public MethodGraph(Set<JoanaVertex> vertices, Set<JoanaEdge> edges, 
+            List<FieldAccess> fieldAccesses, String methodName, Integer id) {
+        super(methodName, id);
+        this.fieldAccesess = fieldAccesses;
+    }
+    
+    /**
+     * Returns the entry vertex of a method.
+     * 
+     * @return The entry vertex of a method.
+     */
+    public JoanaVertex getEntryVertex() { 
+        if (entry == null) {
+            return searchEntry();
+        }
+        return entry;
     }
 
-    private static LayoutRegister register;
-	
-	/**
-	 * Returns the entry vertex of a method.
-	 * 
-	 * @return The entry vertex of a method.
-	 */
-	public JoanaVertex getEntryVertex() { 
-		// TODO Auto-generated method
-		return null;
-	 }
+    private JoanaVertex searchEntry() {
+        for (JoanaVertex v : getVertexSet()) {
+            if (v.getName() == ENTRY_NAME) {
+                entry = v;
+                return entry;
+            }
+        }
+        return null;
+    }
 
-	/**
-	 * Returns a list of all {@link FieldAccess} in the MethodGraph.
-	 * 
-	 * @return A list of all {@link FieldAccess} in the MethodGraph.
-	 */
-	public List<FieldAccess> getFieldAccesses() { 
-		// TODO Auto-generated method
-		return null;
-	 }
 
-	/**
-	 * Returns a list of all {@link JoanaVertex} which are method calls in the MethodGraph.
-	 * 
-	 * @return A list of all method calls.
-	 */
-	public List<JoanaVertex> getMethodCalls() { 
-		// TODO Auto-generated method
-		return null;
-	 } 
+    /**
+     * Returns a list of all {@link FieldAccess} in the MethodGraph.
+     * 
+     * @return A list of all {@link FieldAccess} in the MethodGraph.
+     */
+    public List<FieldAccess> getFieldAccesses() { 
+        return null;
+    }
 
-	/**
-	 * Sets the {@link LayoutRegister}, which stores the available 
-	 * {@link LayoutOption} for all method graphs statically.
-	 * @param register The {@link LayoutRegister} that will be set.
-	 */
-	protected static void setRegister(LayoutRegister register) {
-		MethodGraph.register = register;
-	}
+    /**
+     * Returns a list of all {@link JoanaVertex} which are method calls in the MethodGraph.
+     * 
+     * @return A list of all method calls.
+     */
+    public List<JoanaVertex> getMethodCalls() { 
+        // TODO Auto-generated method
+        return null;
+    }
 
-	@Override
-	public CompoundVertex collapse(Set<JoanaVertex> subset) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    /**
+     * Sets the {@link LayoutRegister}, which stores the available 
+     * {@link LayoutOption} for all method graphs statically.
+     * @param register The {@link LayoutRegister} that will be set.
+     */
+    protected static void setRegister(LayoutRegister register) {
+        MethodGraph.register = register;
+    }
 
-	@Override
-	public Set<JoanaVertex> expand(CompoundVertex vertex) {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public CompoundVertex collapse(Set<JoanaVertex> subset) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public boolean isCompound(Vertex vertex) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+    @Override
+    public Set<JoanaVertex> expand(CompoundVertex vertex) {
+        // TODO Auto-generated method stub
+        return null;
+    }
 
-	@Override
-	public int getLayerWidth(int layerN) {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    @Override
+    public boolean isCompound(Vertex vertex) {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
-	@Override
-	public List<LayeredGraph<JoanaVertex, JoanaEdge>> getSubgraphs() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+    @Override
+    public int getLayerWidth(int layerN) {
+        // TODO Auto-generated method stub
+        return 0;
+    }
+
+    @Override
+    public List<LayeredGraph<JoanaVertex, JoanaEdge>> getSubgraphs() {
+        // TODO Auto-generated method stub
+        return null;
+    }
 }
