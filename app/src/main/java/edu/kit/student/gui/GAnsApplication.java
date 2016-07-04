16b45943b8caa564a5c5b2565561c116191d6b89
Nicolas-Boltz <boltz.nicolas@gmx.de>
2016-07-05 00:20:13 +0200

    updated gui, dialogs and errorhandling/avoidance

--- app/src/main/java/edu/kit/student/gui/GAnsApplication.java
+++ app/src/main/java/edu/kit/student/gui/GAnsApplication.java
@@ -4,12 +4,11 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.text.ParseException;
 import java.util.ArrayList;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Optional;
-
-import javax.swing.filechooser.FileNameExtensionFilter;
 
 import edu.kit.student.graphmodel.Graph;
 import edu.kit.student.graphmodel.GraphModel;
@@ -27,11 +26,16 @@ import javafx.beans.value.ObservableValue;
 import javafx.collections.ListChangeListener;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.geometry.Orientation;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceDialog;
+import javafx.scene.control.Dialog;
 import javafx.scene.control.Menu;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
@@ -46,6 +50,7 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
 import javafx.stage.Stage;
 
 /**
@@ -146,6 +151,8 @@ public class GAnsApplication extends Application {
 				currentGraphView = ((GraphView) group.getChildren().get(group.getChildren().size() - 1));
 			}
 		});
+		
+		
 
 		mainViewLayout.getItems().addAll(graphViewTabPane, treeInfoLayout);
 		rootLayout.getChildren().addAll(menuBar, mainViewLayout);
@@ -161,12 +168,12 @@ public class GAnsApplication extends Application {
 	private void importClicked() {
 		List<Importer> importerList = PluginManager.getPluginManager().getImporter();
 		List<String> supportedFileExtensions = new ArrayList<String>();
-		supportedFileExtensions.toArray();
 		importerList.forEach((importer) -> supportedFileExtensions.add(importer.getSupportedFileEndings()));
-		//FileNameExtensionFilter filter = new FileNameExtensionFilter("Supported file extensions", supportedFileExtensions.toArray(new String[supportedFileExtensions.size()]));
+		ExtensionFilter filter = new ExtensionFilter("Supported file extensions", supportedFileExtensions);
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Select a graph file");
-		//fileChooser.setSelectedExtensionFilter(filter);
+		fileChooser.getExtensionFilters().add(filter);
+		fileChooser.setSelectedExtensionFilter(filter);
 		currentFile = fileChooser.showOpenDialog(primaryStage);
 		if(currentFile == null) return;
 		if(!openWorkspaceDialog()) return;
@@ -174,31 +181,39 @@ public class GAnsApplication extends Application {
 		try {
 			inputStream = new FileInputStream(currentFile);
 			String fileName = currentFile.getName();
-			String fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
+			String fileExtension = "*" + fileName.substring(fileName.lastIndexOf('.'));
 			Importer importer = importerList.get(supportedFileExtensions.indexOf(fileExtension));
 			importer.importGraph(workspace.getGraphModelBuilder(), inputStream);
 			this.model = workspace.getGraphModel(); //TODO: wie wird das model im workspace gesetzt?
-			showGraph(this.model.getGraphs().get(0));
+			Graph currentGraph = this.model.getRootGraphs().get(0);
+			openLayoutSelectionDialog(currentGraph);
+			showGraph(currentGraph);
 			this.structureView.showGraphModel(this.model);
+		} catch (ParseException e) {
+			Alert alert = new Alert(AlertType.ERROR);
+			alert.setHeaderText(null);
+			alert.setContentText(e.getMessage());
+			return;
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		}
+		} 
 	}
 
 	private void exportClicked() {
 		List<Exporter> exporterList = PluginManager.getPluginManager().getExporter();
 		List<String> supportedFileExtensions = new ArrayList<String>();
 		exporterList.forEach((exporter) -> supportedFileExtensions.add(exporter.getSupportedFileEnding()));
-		//FileNameExtensionFilter filter = new FileNameExtensionFilter("Supported file extensions", supportedFileExtensions.toArray(new String[supportedFileExtensions.size()]));
+		ExtensionFilter filter = new ExtensionFilter("Supported file extensions", supportedFileExtensions);
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Select an export location");
-		//fileChooser.setSelectedExtensionFilter(filter);
+		fileChooser.getExtensionFilters().add(filter);
+		fileChooser.setSelectedExtensionFilter(filter);
 		File saveFile = fileChooser.showSaveDialog(primaryStage);
 		try {
 			FileOutputStream outputStream = new FileOutputStream(saveFile);
 			String fileName = saveFile.getName();
-			String fileExtension = fileName.substring(fileName.lastIndexOf('.') + 1);
+			String fileExtension = "*" + fileName.substring(fileName.lastIndexOf('.'));
 			Exporter exporter = exporterList.get(supportedFileExtensions.indexOf(fileExtension));
 			exporter.exportGraph(currentGraphView.getFactory().getGraph().serialize(), outputStream);
 		} catch (FileNotFoundException e) {
@@ -243,6 +258,9 @@ public class GAnsApplication extends Application {
 				importClicked();
 			}
 		});
+		if(PluginManager.getPluginManager().getImporter().isEmpty()) {
+			importItem.setDisable(true);
+		}
 		MenuItem exportItem = new MenuItem("Export");
 		exportItem.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -250,6 +268,9 @@ public class GAnsApplication extends Application {
 				exportClicked();
 			}
 		});
+		if(PluginManager.getPluginManager().getExporter().isEmpty()) {
+			exportItem.setDisable(true);
+		}
 		MenuItem exitItem = new MenuItem("Exit");
 		exitItem.setOnAction(new EventHandler<ActionEvent>() {
 			@Override
@@ -259,24 +280,30 @@ public class GAnsApplication extends Application {
 		});
 		menuFile.getItems().addAll(importItem, exportItem, exitItem);
 
-		Menu menuEdit = new Menu("Layout");
-		Menu changeLayoutItem = new Menu("Layout algorithms");
-		// Experimental ------
-		MenuItem methodGraph = new MenuItem("MethodGraphLayout");
-		changeLayoutItem.getItems().add(methodGraph);
-		changeLayoutItem.setOnAction(new EventHandler<ActionEvent>() {
+		Menu menuLayout = new Menu("Layout");
+		Menu changeLayoutItem = new Menu("Change algorithms");
+		
+		menuLayout.setOnShowing(new EventHandler<Event>() {
 			@Override
-			public void handle(ActionEvent e) {
-//				openParameterDialog(methodlayout.getSettings());
-
+			public void handle(Event e) {
+				changeLayoutItem.getItems().clear();
+				if(currentGraphView != null) {
+					for(LayoutOption option: currentGraphView.getFactory().getGraph().getRegisteredLayouts()) {
+						MenuItem item = new MenuItem(option.getName());
+						item.setOnAction(new EventHandler<ActionEvent>() {
+							@Override
+							public void handle(ActionEvent e) {
+								option.chooseLayout();
+								if(openParameterDialog(option.getSettings())) option.applyLayout();
+							}
+						});
+						changeLayoutItem.getItems().add(item);
+					}
+				}
 			}
 		});
-		// ------------
-		// TODO: In diesem Menue muessen die unterstuetzten Algorithmen
-		// eingfuegt
-		// werden.
 		MenuItem layoutPropertiesItem = new MenuItem("Properties");
-		menuEdit.getItems().addAll(changeLayoutItem, layoutPropertiesItem);
+		menuLayout.getItems().addAll(changeLayoutItem, layoutPropertiesItem);
 
 		Menu menuView = new Menu("View");
 		MenuItem testItem = new MenuItem("test");
@@ -288,34 +315,53 @@ public class GAnsApplication extends Application {
 		});
 		menuView.getItems().add(testItem);
 
-		menuBar.getMenus().addAll(menuFile, menuEdit, menuView);
+		menuBar.getMenus().addAll(menuFile, menuLayout, menuView);
 	}
 
 	private boolean openWorkspaceDialog() {
 		List<String> workspaceNames = new ArrayList<String>();
-		PluginManager.getPluginManager().getWorkspaceOptions().forEach((option) -> workspaceNames.add(option.getName()));
+		List<WorkspaceOption> options = PluginManager.getPluginManager().getWorkspaceOptions();
+		options.forEach((option) -> workspaceNames.add(option.getName()));
 		ChoiceDialog<String> dialog = new ChoiceDialog<String>(workspaceNames.get(0), workspaceNames);
 		dialog.setTitle("Workspaces");
+		dialog.setHeaderText(null);
+		dialog.setGraphic(null);
 		dialog.setContentText("Choose a workspace:");
 		Optional<String> result = dialog.showAndWait();
-		if (result.isPresent()){
-		    WorkspaceOption chosenOption = PluginManager.getPluginManager().getWorkspaceOptions().get(workspaceNames.indexOf(result.get()));
-		    openParameterDialog(chosenOption.getSettings());
-		    workspace = chosenOption.getInstance();
-		    return true;
+		if (result.isPresent()) {
+		    WorkspaceOption chosenOption = options.get(workspaceNames.indexOf(result.get()));
+		    if(openParameterDialog(chosenOption.getSettings())) {
+		    	workspace = chosenOption.getInstance();
+			    return true;
+		    }
 		}
 		return false;
 	}
 	
-	private void openLayoutSelectionDialog() {
-		//TODO: Implementieren und nutzen
+	private boolean openLayoutSelectionDialog(Graph graph) {
+		List<String> layoutNames = new ArrayList<String>();
+		List<LayoutOption> options = graph.getRegisteredLayouts();
+		options.forEach((option) -> layoutNames.add(option.getName()));
+		ChoiceDialog<String> dialog = new ChoiceDialog<String>(layoutNames.get(0), layoutNames);
+		dialog.setTitle("Layout Algorithms");
+		dialog.setHeaderText(null);
+		dialog.setGraphic(null);
+		dialog.setContentText("Choose an layout algorithm:");
+		Optional<String> result = dialog.showAndWait();
+		if (result.isPresent()) {
+		    LayoutOption chosenOption = options.get(layoutNames.indexOf(result.get()));
+		    chosenOption.chooseLayout();
+		    if(openParameterDialog(chosenOption.getSettings())) chosenOption.applyLayout();
+		    return true;
+		}
+		return false;
 	}
 	
 	private void openLayoutSettingsDialog() {
 		//TODO: Implementieren und nutzen
 	}
 
-	private void openParameterDialog(Settings settings) {
+	private boolean openParameterDialog(Settings settings) {
 		GridPane root = new GridPane();
 		ColumnConstraints c1 = new ColumnConstraints();
 		ColumnConstraints c2 = new ColumnConstraints();
@@ -324,9 +370,15 @@ public class GAnsApplication extends Application {
 		root.getColumnConstraints().add(c1);
 		root.getColumnConstraints().add(c2);
 		new ParameterDialogGenerator(root, settings);
-		Stage stage = new Stage();
-		stage.setTitle("Settings");
-		stage.setScene(new Scene(root, 450, 450));
-		stage.show();
+		Alert dialog = new Alert(AlertType.CONFIRMATION);
+		dialog.setTitle("Settings");
+		dialog.setHeaderText(null);
+		dialog.setGraphic(null);
+		dialog.getDialogPane().getChildren().add(root);
+		Optional<ButtonType> result = dialog.showAndWait();
+		if(result.get() != ButtonType.OK) {
+			return false;
+		}
+		return true;
 	}
 }
