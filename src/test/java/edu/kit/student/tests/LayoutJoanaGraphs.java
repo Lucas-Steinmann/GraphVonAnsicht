b88ba80b023c006b69b47444ba374f5a3d918d12
Jonas Fehrenbach <Jonas.Fehrenbach1@web.de>
2016-08-22 17:35:36 +0200

    initialize javafx screen in LayoutJoanaGraphTest

--- src/test/java/edu/kit/student/tests/LayoutJoanaGraphs.java
+++ src/test/java/edu/kit/student/tests/LayoutJoanaGraphs.java
@@ -2,6 +2,8 @@ package edu.kit.student.tests;
 
 import java.util.List;
 
+import javafx.application.Application;
+
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -12,6 +14,7 @@ import edu.kit.student.joana.callgraph.CallGraph;
 import edu.kit.student.joana.callgraph.CallGraphLayout;
 import edu.kit.student.joana.methodgraph.MethodGraph;
 import edu.kit.student.joana.methodgraph.MethodGraphLayout;
+import edu.kit.student.sugiyama.steps.tests.SugiyamaLayoutAlgorithmTest.AsNonApp;
 
 public class LayoutJoanaGraphs {
 
@@ -22,6 +25,14 @@ public class LayoutJoanaGraphs {
     public static void setUpBeforeClass() throws Exception {
          model = JoanaGraphTester.getGraphModelGenerator().get(0);
          plugin = new JoanaPlugin();
+         
+         Thread t = new Thread("JavaFX Init Thread") {
+             public void run() {
+                 Application.launch(AsNonApp.class, new String[0]);
+             }
+         };
+         t.setDaemon(true);
+         t.start();
     }
 
     @Before
