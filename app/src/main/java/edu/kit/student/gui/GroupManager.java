fb6a7520f970f0ddea1986f68275680e10db2da7
Lucas Steinmann <lucas.steinmann@gmail.com>
2017-02-03 10:11:07 +0100

    Clean up GroupManager

--- app/src/main/java/edu/kit/student/gui/GroupManager.java
+++ app/src/main/java/edu/kit/student/gui/GroupManager.java
@@ -9,16 +9,12 @@ import java.util.Set;
 
 import edu.kit.student.graphmodel.ViewableVertex;
 import edu.kit.student.util.LanguageManager;
-import javafx.beans.value.ChangeListener;
-import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.Dialog;
-import javafx.scene.control.DialogEvent;
 import javafx.scene.control.ListCell;
 import javafx.scene.control.ListView;
 import javafx.scene.control.SelectionMode;
@@ -30,26 +26,25 @@ import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
-import javafx.util.Callback;
 
-public class GroupManager {
+class GroupManager {
 
-	private GraphViewGraphFactory factory;
-	
-	private List<Integer> groupIdsBacking;
-	private ObservableList<Integer> groupIds;
-	private Map<Integer, VertexGroup> groupMap;
-	private List<Integer> removedGroups;
+	final private GraphViewGraphFactory factory;
 
-	public GroupManager(GraphViewGraphFactory factory) {
+    private List<Integer> groupIdsBacking;
+    private ObservableList<Integer> groupIds;
+    final private Map<Integer, VertexGroup> groupMap;
+    final private List<Integer> removedGroups;
+
+	GroupManager(GraphViewGraphFactory factory) {
 		this.factory = factory;
-		groupIdsBacking = new LinkedList<Integer>();
+		groupIdsBacking = new LinkedList<>();
 		groupIds = FXCollections.observableList(groupIdsBacking);
-		groupMap = new HashMap<Integer, VertexGroup>();
-		removedGroups = new LinkedList<Integer>();
+		groupMap = new HashMap<>();
+		removedGroups = new LinkedList<>();
 	}
 	
-	public boolean openAddGroupDialog(Set<ViewableVertex> vertices) {
+	boolean openAddGroupDialog(Set<ViewableVertex> vertices) {
 		TextInputDialog dialog = new TextInputDialog(LanguageManager.getInstance().get("wind_group_new_default"));
     	dialog.setTitle(LanguageManager.getInstance().get("wind_group_new_title"));
     	dialog.setHeaderText(null);
@@ -67,72 +62,55 @@ public class GroupManager {
     	return false;
     }
 
-	public void openGroupDialog() {
-		Dialog<ButtonType> dialog = new Dialog<ButtonType>();
-		ListView<Integer> groupList = new ListView<Integer>();
+	void openGroupDialog() {
+		Dialog<ButtonType> dialog = new Dialog<>();
+		ListView<Integer> groupList = new ListView<>();
 		groupList.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
 		groupList.setItems(groupIds);
-		groupList.setCellFactory(new Callback<ListView<Integer>, ListCell<Integer>>() {
-			@Override
-			public ListCell<Integer> call(ListView<Integer> list) {
-				return new GroupListCell();
-			}
-		});
+		groupList.setCellFactory(list -> new GroupListCell());
 		
 		Button upButton = new Button(LanguageManager.getInstance().get("wind_group_up"));
 		upButton.setDisable(true);
-		upButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent e) {
-				Integer groupId = groupList.getSelectionModel().getSelectedItem();
-				int currentPos = groupIds.indexOf(groupId);
-				groupIds.remove(groupId);
-				groupIds.add(currentPos - 1, groupId);
-				groupList.getSelectionModel().select(groupId);
-			}
-		});
+		upButton.setOnAction(event -> {
+            Integer groupId = groupList.getSelectionModel().getSelectedItem();
+            int currentPos = groupIds.indexOf(groupId);
+            groupIds.remove(groupId);
+            groupIds.add(currentPos - 1, groupId);
+            groupList.getSelectionModel().select(groupId);
+        });
 		
 		Button downButton = new Button(LanguageManager.getInstance().get("wind_group_down"));
 		downButton.setDisable(true);
-		downButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent e) {
-				Integer groupId = groupList.getSelectionModel().getSelectedItem();
-				int currentPos = groupIds.indexOf(groupId);
-				groupIds.remove(groupId);
-				groupIds.add(currentPos + 1, groupId);
-				groupList.getSelectionModel().select(groupId);
-			}
-		});
+		downButton.setOnAction(event -> {
+            Integer groupId = groupList.getSelectionModel().getSelectedItem();
+            int currentPos = groupIds.indexOf(groupId);
+            groupIds.remove(groupId);
+            groupIds.add(currentPos + 1, groupId);
+            groupList.getSelectionModel().select(groupId);
+        });
 		
 		Button removeButton = new Button(LanguageManager.getInstance().get("wind_group_remove"));
 		removeButton.setDisable(true);
-		removeButton.setOnAction(new EventHandler<ActionEvent>() {
-			@Override
-			public void handle(ActionEvent e) {
-				Integer groupId = groupList.getSelectionModel().getSelectedItem();
-				groupList.getSelectionModel().clearSelection();
-				groupIds.remove(groupId);
-				removedGroups.add(groupId);
-				groupList.refresh();
-			}
-		});
+		removeButton.setOnAction(event -> {
+            Integer groupId = groupList.getSelectionModel().getSelectedItem();
+            groupList.getSelectionModel().clearSelection();
+            groupIds.remove(groupId);
+            removedGroups.add(groupId);
+            groupList.refresh();
+        });
 		
-		groupList.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<Integer>() {
-			@Override
-			public void changed(ObservableValue<? extends Integer> observable, Integer oldValue, Integer newValue) {
-				if(newValue == null) {
-					upButton.setDisable(true);
-					downButton.setDisable(true);
-					removeButton.setDisable(true);
-				} else {
-					int index = groupIds.indexOf(newValue);
-					upButton.setDisable(index == 0);
-					downButton.setDisable(index == groupIds.size() - 1);
-					removeButton.setDisable(false);
-				}
-			}
-		});
+		groupList.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if(newValue == null) {
+                upButton.setDisable(true);
+                downButton.setDisable(true);
+                removeButton.setDisable(true);
+            } else {
+                int index = groupIds.indexOf(newValue);
+                upButton.setDisable(index == 0);
+                downButton.setDisable(index == groupIds.size() - 1);
+                removeButton.setDisable(false);
+            }
+        });
 		
 		HBox buttonBox = new HBox(upButton, downButton, removeButton);
 		buttonBox.setSpacing(3);
@@ -146,8 +124,8 @@ public class GroupManager {
 		Stage stage = (Stage) dialog.getDialogPane().getScene().getWindow();
     	stage.getIcons().add(new Image("gans_icon.png"));
 
-		LinkedList<Integer> groupIdsAbortBackup = new LinkedList<Integer>(this.groupIds);
-		Map<Integer,Color> groupColorAbortBackup = new HashMap<Integer,Color>();
+		LinkedList<Integer> groupIdsAbortBackup = new LinkedList<>(this.groupIds);
+		Map<Integer,Color> groupColorAbortBackup = new HashMap<>();
 		for(Integer id : groupMap.keySet()) {
 			groupColorAbortBackup.put(id,groupMap.get(id).getColor());
 		}
@@ -161,7 +139,7 @@ public class GroupManager {
          });
 		Optional<ButtonType> result = dialog.showAndWait();
 		if(result.isPresent()) {
-			if(result.get() == ButtonType.OK || result.get() == ButtonType.APPLY) {
+			if(result.get() == ButtonType.OK) {
 				removedGroups.forEach(groupId -> groupMap.remove(groupId).uncolorVertices());
 				removedGroups.clear();
 				//TODO: maybe check for made changes and only apply them.
@@ -169,7 +147,7 @@ public class GroupManager {
 			}
 		} else {
 			removedGroups.clear();
-			this.groupIdsBacking = new LinkedList<Integer>(groupIdsAbortBackup);
+			this.groupIdsBacking = new LinkedList<>(groupIdsAbortBackup);
 			this.groupIds = FXCollections.observableList(groupIdsBacking);
 			for(Integer id : groupColorAbortBackup.keySet()) {
 				groupMap.get(id).setColor(groupColorAbortBackup.get(id));
@@ -177,7 +155,7 @@ public class GroupManager {
 		}
 	}
 	
-	public void applyGroups() {
+	void applyGroups() {
 		//TODO: inefficient, could map over all groups and vertices before coloring
 		for(int i = groupIds.size() - 1; i > -1; i--) {
 			groupMap.get(groupIds.get(i)).colorVertices();
